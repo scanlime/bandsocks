d722f1ec37b1e2f85e24277e4112b2a5c99c7c53
Micah Elizabeth Scott <micah@scanlime.org>
2020-10-01 10:28:05 -0700

    cleanup in the sandbox

--- sand/src/main.rs
+++ sand/src/main.rs
@@ -3,55 +3,35 @@
 #[cfg(not(any(target_os = "linux", target_os = "android")))]
 compile_error!("bandsocks only works on linux or android");
 
+mod seccomp;
+mod tracer;
+
 use pentacle::ensure_sealed;
-use pete::{Command, Ptracer, Restart};
-use syscallz::{Context, Action, Syscall};
+use std::process::Command;
 use std::os::unix::process::CommandExt;
 
-// To do:  This kinda works, but 'pete' is crashing due to a PID we've never heard of, and the libseccomp dependency is annoying. Let's ditch those and put in something lower-level, and try to trace a subset of syscalls from this process.
+pub mod modes {
+    pub const STAGE_1_TRACER: &'static str = "sand";
+    pub const STAGE_2_LOADER: &'static str = "sand-exec";
+}
 
 fn main() {
     ensure_sealed().unwrap();
-    println!("sand {:?}", std::env::args());
+    let mode = std::env::args().next();
 
-    if let Some(argv0) = std::env::args().next() {
-        if argv0 == "sand" {
-            do_seccomp();
-            do_ptrace();
-        } else {
-            do_shell();
-        }
+    if mode == Some(modes::STAGE_1_TRACER.to_string()) {
+        seccomp::activate();
+        tracer::run();
+
+    } else if mode == Some(modes::STAGE_2_LOADER.to_string()) {
+        // Start the sandboxed binary here once we have a sandbox. For now it's a shell to inspect the world.
+        Command::new("/bin/sh").exec();
+
+    } else {
+        // Started under unknown conditions... this shouldn't happen when we're in the runtime,
+        // but this is where we end up when running the binary manually for testing.
+        // Restart as the stage 1 tracer.
+        println!("hi.");
+        Command::new("/proc/self/exe").arg0(modes::STAGE_1_TRACER);
     }
-}
-
-fn do_shell() {
-    std::process::Command::new("/bin/sh").exec();
-}
-
-fn do_ptrace() {
-    let argv = vec![ b"/proc/self/exe".to_vec() ];
-    let cmd = Command::new(argv).unwrap();
-    let mut ptracer = Ptracer::new();
-
-    let tracee = ptracer.spawn(cmd).unwrap();
-    ptracer.restart(tracee, Restart::Continue).unwrap();
-
-    while let Some(tracee) = ptracer.wait().unwrap() {
-        let regs = tracee.registers().unwrap();
-        let pc = regs.rip as u64;
-
-        println!("{:>16x}: {:?}", pc, tracee.stop);
-
-        ptracer.restart(tracee, Restart::Continue).unwrap();
-    }
-}
-
-fn do_seccomp() {
-    let mut ctx = Context::init_with_action(Action::Allow).unwrap();
-
-    ctx.set_action_for_syscall(Action::Trace(1234), Syscall::clock_nanosleep);
-
-    println!("pre-seccomp");
-    ctx.load().unwrap();
-    println!("post-seccomp");
 }
