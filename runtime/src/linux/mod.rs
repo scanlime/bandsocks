1efbfbf9338f381a9f301d34687b7fb7f403a013
Micah Elizabeth Scott <micah@scanlime.org>
2020-09-30 19:47:25 -0700

    try a ptrace inside the sand

--- runtime/src/linux/mod.rs
+++ runtime/src/linux/mod.rs
@@ -1,7 +1,6 @@
 // This code may not be used for any purpose. Be gay, do crime.
 
 use pentacle::SealedCommand;
-use pete::{Command, Ptracer, Restart};
 use std::io::Cursor;
 use std::os::unix::process::CommandExt;
 
@@ -13,23 +12,7 @@ pub fn do_the_thing() {
     let mut elf_reader = Cursor::new(ELF);
     let mut cmd = SealedCommand::new(&mut elf_reader).unwrap();
     cmd.arg0("sand");
+
     println!("{:?}", cmd.status());
-
-    /*    
-    let argv = vec![ b"/dev/null".to_vec() ];
-    let cmd = Command::new(argv).unwrap();
-    let mut ptracer = Ptracer::new();
-
-    let tracee = ptracer.spawn(cmd).unwrap();
-    ptracer.restart(tracee, Restart::Continue).unwrap();
-
-    while let Some(tracee) = ptracer.wait().unwrap() {
-        let regs = tracee.registers().unwrap();
-        let pc = regs.rip as u64;
-
-        println!("{:>16x}: {:?}", pc, tracee.stop);
-
-        ptracer.restart(tracee, Restart::Continue).unwrap();
-    */
 }
 
--- sand/src/main.rs
+++ sand/src/main.rs
@@ -1,8 +1,42 @@
 // This code may not be used for any purpose. Be gay, do crime.
 
-use std::process::Command;
+use pentacle::ensure_sealed;
+use pete::{Command, Ptracer, Restart};
 
 fn main() {
-    println!("sand");
-    Command::new("/bin/sh").status().unwrap();
+    ensure_sealed().unwrap();
+
+    println!("sand {:?}", std::env::args());
+    std::thread::sleep(std::time::Duration::from_secs(1));
+
+    if let Some(argv0) = std::env::args().next() {
+
+        if argv0 == "sand" {
+            do_ptrace();
+        } else {
+            do_shell();
+        }
+    }
+}
+
+fn do_shell() {
+    std::process::Command::new("/bin/sh").status().unwrap();
+}
+
+fn do_ptrace() {
+    let argv = vec![ b"/proc/self/exe".to_vec() ];
+    let cmd = Command::new(argv).unwrap();
+    let mut ptracer = Ptracer::new();
+
+    let tracee = ptracer.spawn(cmd).unwrap();
+    ptracer.restart(tracee, Restart::Continue).unwrap();
+
+    while let Some(tracee) = ptracer.wait().unwrap() {
+        let regs = tracee.registers().unwrap();
+        let pc = regs.rip as u64;
+
+        println!("{:>16x}: {:?}", pc, tracee.stop);
+
+        ptracer.restart(tracee, Restart::Continue).unwrap();
+    }
 }
