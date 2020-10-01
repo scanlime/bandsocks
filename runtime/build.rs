56b1c5bcd730b55806dfba31801909a312a56274
Micah Elizabeth Scott <micah@scanlime.org>
2020-10-01 09:21:53 -0700

    build script dependency fix

--- runtime/build.rs
+++ runtime/build.rs
@@ -4,12 +4,13 @@ use build_deps::rerun_if_changed_paths;
 
 fn main() {
     let out_dir = std::env::var("OUT_DIR").unwrap();
-    let sand_crate = Path::new("../sand");
     let sand_target = Path::new(&out_dir).join("sand-target");
-    
-    rerun_if_changed_paths( sand_crate.join("**").to_str().unwrap() ).unwrap();
+
+    rerun_if_changed_paths("../sand/*").unwrap();
+    rerun_if_changed_paths("../sand/src/*").unwrap();   
+
     assert!(Command::new("cargo")
-            .current_dir(sand_crate)
+            .current_dir("../sand")
             .arg("build")
             .arg("--release")
             .arg("--target-dir").arg(sand_target)
