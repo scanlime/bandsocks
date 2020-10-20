3a3e1c5d4e7723fe4eaad6a6de02d9e0afa4850d
Micah Elizabeth Scott <micah@scanlime.org>
2020-10-20 16:30:10 -0700

    debug from yesterday

--- sand/src/process/loader.rs
+++ sand/src/process/loader.rs
@@ -27,6 +27,8 @@ impl<'q, 's, 't> Loader<'q, 's, 't> {
         // temp: testing out remote syscalls and memory access so we can build a loader
         println!("made it to exec! with {:x?}", self.stopped_task);
 
+        remote::print_maps(self.stopped_task);
+
         let saved_regs = self.stopped_task.regs.clone();
         let scratch_ptr = VPtr(((self.stopped_task.regs.sp & !0xFFF) - 0x2000) as usize);
 
@@ -48,7 +50,6 @@ impl<'q, 's, 't> Loader<'q, 's, 't> {
             0xffffffffff600000 as usize as isize, 0x1000
         ]).await;
         assert_eq!(reply, 0);
-        remote::print_maps(self.stopped_task);
 
         for n in 0u8..100u8 {
             *self.stopped_task.regs = saved_regs.clone();
