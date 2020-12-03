e8523fd5ba1f3af81fc99bae0365342a902399e8
Micah Elizabeth Scott <micah@scanlime.org>
2020-12-03 15:54:40 -0800

    support both rustup cargo and env cargo in builder

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ use build_deps::rerun_if_changed_paths;
 use std::{env::var, path::Path, process::Command};
 
 fn main() {
+    let cargo = var("CARGO").unwrap();
     let out_dir = var("OUT_DIR").unwrap();
     let sand_target = Path::new(&out_dir).join("sand-target");
 
@@ -9,18 +10,34 @@ fn main() {
     rerun_if_changed_paths("sand/src/**/*.rs").unwrap();
 
     // skip building sand when running in rust-language-server
-    let skip_sand_build = var("RUSTC_WORKSPACE_WRAPPER").is_ok();
-
-    if !skip_sand_build {
-        assert!(Command::new("cargo")
-            .current_dir("sand")
-            .arg("+nightly")
-            .arg("build")
-            .arg("--release")
-            .arg("--target-dir")
-            .arg(sand_target)
-            .status()
-            .unwrap()
-            .success());
+    if var("RUSTC_WORKSPACE_WRAPPER").is_ok() {
+        return;
     }
+
+    let sand_dir = "sand";
+    let args = &[
+        "build",
+        "--release",
+        "--target-dir",
+        sand_target.to_str().unwrap(),
+    ];
+
+    // prefer to run rustup's cargo wrapper and explicitly ask for nightly.
+    // sand needs nightly rust but we'd like to not require it for the outer app.
+    let result = Command::new("cargo")
+        .current_dir(sand_dir)
+        .arg("+nightly")
+        .args(args)
+        .status();
+    if result.is_ok() {
+        assert!(result.unwrap().success());
+        return;
+    }
+
+    assert!(Command::new(cargo)
+        .current_dir(sand_dir)
+        .args(args)
+        .status()
+        .unwrap()
+        .success());
 }
