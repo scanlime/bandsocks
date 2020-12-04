eb862c352f88e2b49f33a823faf61a3dfcf3f5ac
Micah Elizabeth Scott <micah@scanlime.org>
2020-12-03 19:59:46 -0800

    fix nested build

--- build.rs
+++ build.rs
@@ -1,6 +1,11 @@
 use build_deps::rerun_if_changed_paths;
 use fs_extra::{copy_items, dir::CopyOptions};
-use std::{env::var, path::Path, process::Command};
+use std::{
+    env::var,
+    fs::{copy, create_dir_all},
+    path::Path,
+    process::Command,
+};
 
 fn main() {
     // skip building sand when running in rust-language-server
@@ -11,11 +16,18 @@ fn main() {
     let cargo = var("CARGO").unwrap();
     let out_dir = var("OUT_DIR").unwrap();
     let build_dir = Path::new(&out_dir).join("sand");
+    create_dir_all(&build_dir).unwrap();
 
     rerun_if_changed_paths("sand/sand-Cargo.toml").unwrap();
     rerun_if_changed_paths("sand/sand-Cargo.lock").unwrap();
     rerun_if_changed_paths("sand/src/**/*.rs").unwrap();
-    copy_items(&["sand"], out_dir, &CopyOptions::new()).unwrap();
+
+    let mut opts = CopyOptions::new();
+    opts.copy_inside = true;
+    opts.skip_exist = true;
+    copy("sand/sand-Cargo.toml", build_dir.join("Cargo.toml")).unwrap();
+    copy("sand/sand-Cargo.lock", build_dir.join("Cargo.lock")).unwrap();
+    copy_items(&["sand/src"], &build_dir, &opts).unwrap();
 
     let args = &["build", "--release"];
 
